039b219cc22e27fd173b5399526d22e44f733667
oma-umeh <helenumeh21@gmail.com>
2023-07-31 10:09:27 +0100

    fix: tribute fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    
+    screens: {
+      "xxs": "300px",
+      "xs": "400px",
+      // => @media (min-width: 420px) { ... }
+      //from 420px upward
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
